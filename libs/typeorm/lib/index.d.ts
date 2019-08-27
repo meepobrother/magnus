107b1baaf24889b7d12acf666f7270e28bbd2809
imeepos <1037483576@qq.com>
2019-08-27 14:37:02 +0800

    fix: 🐛 fix some error

--- libs/typeorm/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ObjectType, EntitySchema, FindManyOptions, EntityManager } from 'typeorm';
-export declare class Typeorm<Entity> extends EntityManager {
-    find<Entity>(entityClass: ObjectType<Entity> | EntitySchema<Entity> | string, optionsOrConditions?: FindManyOptions<Entity> | any): Promise<Entity[]>;
-}
--- libs/typeorm/lib/index.js
+++ libs/typeorm/lib/index.js
@@ -1,15 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-const FindOptionsUtils_1 = require("typeorm/find-options/FindOptionsUtils");
-class Typeorm extends typeorm_1.EntityManager {
-    async find(entityClass, optionsOrConditions) {
-        const metadata = this.connection.getMetadata(entityClass);
-        const qb = this.createQueryBuilder(entityClass, FindOptionsUtils_1.FindOptionsUtils.extractFindManyOptionsAlias(optionsOrConditions) || metadata.name);
-        if (!FindOptionsUtils_1.FindOptionsUtils.isFindManyOptions(optionsOrConditions) || optionsOrConditions.loadEagerRelations !== false)
-            FindOptionsUtils_1.FindOptionsUtils.joinEagerRelations(qb, qb.alias, metadata);
-        return FindOptionsUtils_1.FindOptionsUtils.applyFindManyOptionsOrConditionsToQueryBuilder(qb, optionsOrConditions).getMany();
-    }
-}
-exports.Typeorm = Typeorm;
 //# sourceMappingURL=index.js.map
