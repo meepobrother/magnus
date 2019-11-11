6d353c036580905d91c0d2810a843801a871e78d
imeepos <1037483576@qq.com>
2019-11-11 16:25:48 +0800

    fix: 🐛 save json

--- libs/core/lib/base.ts
+++ libs/core/lib/base.ts
@@ -1,12 +1,3 @@
-import { client } from "@notadd/magnus-graphql";
-import { Where } from "./types";
-import { FindOperator } from "typeorm";
-interface TypeDef {
-    [key: string]: { name: string; decorators: string[]; entity: string }[];
-}
-interface Entities {
-    [key: string]: Type<any>;
-}
 interface Type<T> extends Function {
     new(...args: any[]): T;
 }
@@ -15,12 +6,6 @@ export abstract class MagnusBase<T = any> {
     tablename: string;
     init(tablename: string) {
         this.tablename = this.tablename || tablename;
-    }    
-    createEntity(entity?: Partial<T>) {
-        return new this.entity(entity);
-    }
-    createEntities(entities: Partial<T>[]) {
-        return entities.map(entity => this.createEntity(entity));
     }
 }
 
